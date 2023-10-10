dba869df41220e2faa46d5606f7e034f37b99e37
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-10 16:30:50 +0100

    push

--- container/node-express-typescript-starter-main/src/index.ts
+++ container/node-express-typescript-starter-main/src/index.ts
@@ -1,10 +1,13 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import 'dotenv/config'
 import * as middleware from './middleware'
 // import { getRandomHadith } from 'sacred_texts'
+import { appendFeed } from 'super_ez_rss'
+import {  retrieveFeed } from 'super_ez_rss'
 import articlesRouter from './routers/articles.router'
+// import chronRouter from './routers/chron.router'
 
 const PORT = process.env.PORT || 8910
 const ENV = process.env.NODE_ENV || 'production'
@@ -19,36 +22,47 @@ app.use(express.json())
 
 app.use(middleware.httpLogger)
 
-// app.get('/', (req, res) => {
-// //   res.status(200).send(JSON.stringify(async () => {
-// // await getRandomHadith()
-// //   }))
-// })
-
-// async function get(){
-//   console.log('hi')
-//   console.log(await getRandomHadith({language:'english'}))
-// }
-// get()
-// Articles routes
 
 app.use('/articles', articlesRouter)
 
-app.get('/:language/:type/', (req:Request,res:Response) => {
- const { language, type } = req.params
-  // const xmlFilePath = path.join(__dirname, 'data', 'feeds', language, `${type}.xml`)
-  // res.type('xml').sendFile(xmlFilePath)
-  console.log(language,type)
-    res.status(200).send({language,type})
+
+app.get('/:language/:type/', async (req, res) => {
+  try {
+    const feed = await retrieveFeed('868404c7-2bb3-4c07-b480-4324b6f792a5')
+
+    if (!feed) {
+      return res.status(404).send('Feed not found')// Handle feed not found scenario
+    } else {
+    console.log('feed found', feed)
+
+    // Send the file using the absolute path
+    res.type('xml').send(feed)
+    }
+
+    // return res.status(200).send('Feed found') // Handle feed found scenario
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error') // Handle other errors
+  }
 })
 
-// Error hanlding middleware
+let newFeed = appendFeed('868404c7-2bb3-4c07-b480-4324b6f792a5', 'new', 'https://google.com', 'this is desc', new Date().toISOString())
+console.log(newFeed)
+
+// app.get('/chron', async () =>
+// {
+// let newFeed = await appendFeed('868404c7-2bb3-4c07-b480-4324b6f792a5', 'new', 'https://google.com', 'this is desc', new Date().toISOString())
+// console.log(newFeed)
+// })
+
+// Error handling middleware
 
 app.use(middleware.errorHandler)
 
 app.use(middleware.notFoundHandler)
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, () =>
+{
   console.log(`Server running on port ${PORT} in ${ENV} environment`)
 })
 
