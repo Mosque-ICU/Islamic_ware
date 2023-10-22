024bea9e90aae47890a0b839a598ad6fa26e3176
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-22 13:52:41 +0100

    remove mobile folder

--- mobile/salat/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  basePath: '',
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  swcMinify: true,
-  transpilePackages: ['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons'],
-}
